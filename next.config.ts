62fadd6d7eebdf57dab0884c6fc4fb80c7b77cd4
EvilChookie <869063+EvilChookie@users.noreply.github.com>
2026-03-15 16:23:56 +1100

    Issue 3: Add a version number to the UI with a github link

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
+import packageJson from "./package.json";
 
 const nextConfig: NextConfig = {
   output: 'standalone',
   devIndicators: {
     position: 'bottom-right'
-  }
+  },
+  env: {
+    APP_VERSION: packageJson.version,
+  },
 };
 
 export default nextConfig;
